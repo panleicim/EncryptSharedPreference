3c004b6cf3ce2737f937bfbcc19b3dd8cae02ce7
Lei PAN <lei_pan@carrefour.com>
2019-11-25 18:38:58 +0100

    Feat: added RecyclerView on the main activity

--- app/src/main/java/com/lpa/encryptedsharedpreferences/MainActivity.kt
+++ app/src/main/java/com/lpa/encryptedsharedpreferences/MainActivity.kt
@@ -2,8 +2,13 @@ package com.lpa.encryptedsharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val r: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.userList)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
