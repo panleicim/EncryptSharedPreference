0d50b70f4a22bd9885876f74641b7b1feaa6b186
PAN Lei <panleicim@gmail.com>
2019-11-26 21:50:39 +0100

    Feat: added CreateUserActivity

---
+++ app/src/main/java/com/lpa/encryptedsharedpreferences/CreateUserActivity.kt
@@ -0,0 +1,14 @@
+package com.lpa.encryptedsharedpreferences
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateUserActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_user_activity)
+    }
+}
--- app/src/main/java/com/lpa/encryptedsharedpreferences/UserListActivity.kt
+++ app/src/main/java/com/lpa/encryptedsharedpreferences/UserListActivity.kt
@@ -2,16 +2,31 @@ package com.lpa.encryptedsharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class UserListActivity : AppCompatActivity() {
 
     val r: RecyclerView by lazy {
         findViewById<RecyclerView>(R.id.userList)
     }
+    val btnAddUser: Button by lazy {
+        findViewById<Button>(R.id.btnAddUser)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpListeners()
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setUpListeners() {
+        btnAddUser.setOnClickListener {
+            showCreateUserActivity()
+        }
+    }
+
+    private fun showCreateUserActivity() {
+
     }
 }
